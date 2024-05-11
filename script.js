6287df11f108c08ac14efcb03e699dd0f52afdbe
vansAP27 <redmianew433@gmail.com>
2024-05-11 19:04:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector
+('.na');
+
+// ketika humberger menu di klik
+
+document.querySelector('#hh').
+onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+
+ //Klik di luar sidebar untuk mengilangkan nav
